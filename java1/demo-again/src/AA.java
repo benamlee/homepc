7921119bb5c5ec655b360637d38444533de4db9d
Ben <leehonamben@gmail.com>
2023-08-16 22:52:35 +0800

    again

---
+++ java1/demo-again/src/AA.java
@@ -0,0 +1,12 @@
+public class AA {
+  public static void main(String[] args) {
+    System.out.println("iasdoashd");
+    for (int i = 0; i < 100; i++) {
+      System.out.println(i);
+      if (i == 55) {
+        for (int j = 0; j < 1000; j++)
+          System.out.println("ho");
+      }
+    }
+  }
+}
